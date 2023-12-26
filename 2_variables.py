fa90f01ab6464024587518d8abc7ba7dad112449
Adlihakim11 <143002365+Adlihakim11@users.noreply.github.com>
2023-12-26 13:24:35 +0800

    secondpython.py
    second python script

---
+++ 2_variables.py
@@ -0,0 +1,13 @@
+rent = 1220
+gas = 202.5
+groceries = 305.6
+print(rent)
+total = rent + gas + groceries
+print(total)
+
+rent = 1400
+item1 = "rent"
+item2 = "gas"
+item3 = "groceries"
+
+print("Expenses Item: ", item1,item2,item3)
